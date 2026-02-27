55609187347b8141145d9896be681adf02978138
Artem Kutsan <artemkutsan@gmail.com>
2026-02-27 10:51:57 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="container">React Summary 5</h1>
+
+        <section>
+          <div className="container">
+            <h2>Задача 1: Асинхронная проверка при регистрации</h2>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
